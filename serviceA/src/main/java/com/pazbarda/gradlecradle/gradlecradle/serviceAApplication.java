262cb753430cd3862dd426476bbc6167faa45a8f
Paz Barda <paz.barda@intel.com>
2024-09-27 21:29:51 +0300

    Gradle project with 1 service, deployed with docker compose

---
+++ serviceA/src/main/java/com/pazbarda/gradlecradle/gradlecradle/serviceAApplication.java
@@ -0,0 +1,13 @@
+package com.pazbarda.gradlecradle.gradlecradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class serviceAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(serviceAApplication.class, args);
+	}
+
+}
---
+++ serviceA/src/test/java/com/pazbarda/gradlecradle/gradlecradle/GradlecradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pazbarda.gradlecradle.gradlecradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradlecradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
